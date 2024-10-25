2156c3950bef9df306b8b685a6234181d4429660
tiansang <zhanghaotian0824@163.com>
2024-10-25 08:15:33 +0800

    Auto Commit

--- push.bat
+++ push.bat
@@ -6,12 +6,10 @@ git status
 
 echo ----- ----- Add changed files ----- -----
 git add .
-echo;
 echo ----- ----- Add ended ----- -----
 
 echo echo ----- ----- Commit files ----- -----
 git commit -m "Auto Commit"
-echo;
 echo ----- ----- Commit ended ----- -----
 
 
