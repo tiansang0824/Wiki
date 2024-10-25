012c69cf0026d86410118dd9cedc3b0a5353a55e
tiansang <zhanghaotian0824@163.com>
2024-10-25 08:16:55 +0800

    Auto Commit

--- push.bat
+++ push.bat
@@ -1,13 +1,16 @@
 @echo off
 
+:: 首先检查更新情况
 echo ----- ----- Start auto script, the current dir is: %cd% ----- -----
 echo ----- ----- Check status ----- -----
 git status
 
+:: 然后添加文件
 echo ----- ----- Add changed files ----- -----
 git add .
 echo ----- ----- Add ended ----- -----
 
+:: 然后提交更新
 echo ----- ----- Commit files ----- -----
 git commit -m "Auto Commit"
 echo ----- ----- Commit ended ----- -----
